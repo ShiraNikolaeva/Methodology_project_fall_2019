5f4e22d9c16d1d7f14e3da3eb9ef99d04807de93
Shira Nikoaleva <shiranikolaeva@gmail.com>
2019-11-05 11:29:23 -0500

    Received Child class, created Parent class and Time class

---
+++ src/main/java/NikalayevaPfeiffer/Child.java
@@ -0,0 +1,97 @@
+package NikalayevaPfeiffer;
+
+
+import java.time.LocalDate;
+
+public class Child {
+	private String firstName;
+	private String lastName;
+	private Time timeInPlaygroup;
+	private boolean transportation;
+	private String allergies;
+	private LocalDate DOB;
+	private Parent parent;
+	private String emergencyContact;
+
+public Child(String firstName, String lastName, Time timeInPlaygroup, String allergies, LocalDate DOB, Parent parent, String emergencyContact) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.timeInPlaygroup = timeInPlaygroup;
+	this.allergies = allergies;
+	this.DOB = DOB;
+	this.parent = parent;
+	this.emergencyContact = emergencyContact;
+}
+public Child(String firstName, String lastName, Time timeInPlaygroup, boolean transportation, String allergies, LocalDate DOB, Parent parent, String emergencyContact) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.timeInPlaygroup = timeInPlaygroup;
+	this.transportation = transportation;
+	this.allergies = allergies;
+	this.DOB = DOB;
+	this.parent = parent;
+	this.emergencyContact = emergencyContact;
+	
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public void setTimeInPlaygroup (Time timeInPlaygroup) {
+	this.timeInPlaygroup = timeInPlaygroup;
+}
+public void setTransportation(boolean transportation) {
+	this.transportation = transportation;
+}
+public void setAllergies(String allergies) {
+	this.allergies = allergies;
+}
+public void setDOB(LocalDate DOB) {
+	this.DOB = DOB;
+}
+public void setParent (Parent parent) {
+	this.parent = parent;
+}
+public void setEmergencyContact(String emergencyContact) {
+	this.emergencyContact = emergencyContact;
+}
+public String getFirstName() {
+	return firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public Time getTimeInPlaygroup() {
+	return timeInPlaygroup;
+}
+public boolean getTransportation() {
+	return transportation;
+}
+public String getAllergies() {
+	return allergies;
+}
+public LocalDate getDOB() {
+	return DOB;
+}
+public Parent getParent() {
+	return parent;
+}
+public String getEmergencyContact() {
+	return emergencyContact;
+}
+@Override
+public String toString() {
+	StringBuilder sb = new StringBuilder();
+	sb.append("\t Name: " + firstName + lastName);
+	sb.append("\t Time In Playgroup: " + timeInPlaygroup);
+	sb.append("\t Transportation: " + transportation);
+	sb.append("\t Allergy Information: " + allergies);
+	sb.append("\t Date of Birth: " + DOB);
+	sb.append("\t Parent Information: " + parent.toString());
+	sb.append("\t Emergency Contact: " + emergencyContact);
+	return sb.toString();
+}
+}
+
---
+++ src/main/java/NikalayevaPfeiffer/Parent.java
@@ -0,0 +1,51 @@
+package NikalayevaPfeiffer;
+
+public class Parent {
+	
+	private String firstName,lastName,phoneNum,address;
+	
+	public Parent(String fName,String lName,String phoneNum,String address) {
+		setFirstName(fName);
+		setLastName(lName);
+		setPhoneNum(phoneNum);
+		setAddress(address);
+	}
+	
+	//setters
+	public void setFirstName(String fName) {
+		this.firstName=fName;
+	}
+	public void setLastName(String lName) {
+		this.lastName=lName;
+	}
+	public void setPhoneNum(String num) {
+		this.phoneNum=num;
+	}
+	public void setAddress(String address) {
+		this.address=address;
+	}
+	
+	//getters
+	public String getFirstName() {
+		return this.firstName;
+	}
+	public String getLastName() {
+		return this.lastName;
+	}
+	public String getPhoneNum() {
+		return this.phoneNum;
+	}
+	public String getAddress() {
+		return this.address;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\t Name: " + firstName + lastName);
+		sb.append("\t Phone Number: "+ phoneNum);
+		sb.append("\t Address: "+address);
+		return sb.toString();
+	}
+
+}
